13535046ef3f17d7f918d457d9f8652fed3fe284
Lewis Collins <lewiscollins@Lewiss-MacBook-Pro.local>
2021-10-17 15:22:28 +0100

    feat: having issues getting it to run on docker-compose up, also receiving new errors when testing after adding back in sql db

--- Cybersmart/settings.py
+++ Cybersmart/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'Cybersmart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cybersmart_db',
+#         'USER': 'cybersmart_dbuser',
+#         'PASSWORD': 'HsndgkqxIr',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'weathercs',
-        'USER': 'lewisc',
-        'PASSWORD': 'HsndgkqxIr',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
